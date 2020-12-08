6d56817a14c0ff116e3acad2f3a21e8f62d31cc7
Guadalupe Vazquez <guvadlt@gmail.com>
2020-12-08 00:18:27 -0600

    Se añadio api de google para registro

---
+++ js/registro.js
@@ -0,0 +1,27 @@
+function onSuccess(googleUser) {
+    $("#email").val(googleUser.getBasicProfile().getEmail());
+    $("#firstName").val(googleUser.getBasicProfile().getGivenName());
+    $("#lastName").val(googleUser.getBasicProfile().getFamilyName());
+
+    }
+    function onFailure(error) {
+    console.log(error);
+    }
+    function renderButton() {
+    gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+    });
+    }
+
+    function onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+      }
